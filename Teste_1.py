f44319226330bc002bf87ec406fca8a8d54710d8
IsabelFrota <isabel@lit.ifce.edu.br>
2017-01-24 12:53:09 -0300

    Método que lê e mostra uma imagem

--- Teste_1.py
+++ Teste_1.py
@@ -1,5 +1,12 @@
 import numpy as np
 import cv2
 
-def MetodoNada():
-    pass
+def CarregarImagem(jpeg):
+    img = cv2.imread(jpeg)
+    cv2.imshow("Lena", img)
+    cv2.waitKey(0)
+
+
+jpg = "len_top.jpg"
+CarregarImagem(jpg)
+
